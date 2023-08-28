751d3c21562000a72837d6df75fbc56b070511a4
narendrabekkem <110079709+narendrabekkem@users.noreply.github.com>
2023-08-28 20:15:45 +0530

    Update GlideDateTimeUsage.js

--- GlideDateTimeUsage.js
+++ GlideDateTimeUsage.js
@@ -1,1 +1,22 @@
+//Add Seconds to GlideDateTimeObject
+gs.info("Time of execution:"+ new GlideDateTime()+"\n TimeZone of the session:"+ gs.getSession().getTimeZoneName());
+var gd2=new GlideDateTime(); //new DateTime object with current time
+gd2.addSeconds(600); //adds 600 seconds
+gs.info(gd2);
 
+//Output
+*** Script: Time of execution:2023-08-28 14:40:45
+ TimeZone of the session:Europe/Amsterdam
+*** Script: 2023-08-28 14:50:45
+
+
+//Add MilliSecond to GlideDateTimeObject
+gs.info("Time of execution:"+ new GlideDateTime()+"\n TimeZone of the session:"+ gs.getSession().getTimeZoneName());
+var gd2=new GlideDateTime();
+gd2.add(600000);
+gs.info(gd2);
+
+//Output
+*** Script: Time of execution:2023-08-28 14:44:42
+ TimeZone of the session:Europe/Amsterdam
+*** Script: 2023-08-28 14:54:42
